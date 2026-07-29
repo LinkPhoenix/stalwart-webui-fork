3f31a115c91927dee93bfc7c1001a0b3af6d6b4e
Steven RYDELL <srydell@gmx.com>
2026-07-29 07:34:48 +0200

    Show an ESC hint instead of the close button in the command palette

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -31,7 +31,7 @@ Command.displayName = CommandPrimitive.displayName;
 const CommandDialog = ({ children, ...props }: DialogProps) => {
   return (
     <Dialog {...props}>
-      <DialogContent className="overflow-hidden p-0">
+      <DialogContent className="overflow-hidden p-0" showCloseButton={false}>
         <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           {children}
         </Command>
@@ -54,6 +54,9 @@ const CommandInput = React.forwardRef<
       )}
       {...props}
     />
+    <kbd className="pointer-events-none ml-2 inline-flex h-5 shrink-0 select-none items-center rounded-md border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground">
+      ESC
+    </kbd>
   </div>
 ));
 CommandInput.displayName = CommandPrimitive.Input.displayName;
